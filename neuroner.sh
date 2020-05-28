070d611b273118ab15b23a02aab19120391600b8
satadisha <ssahabh1@binghamton.edu>
2020-05-28 13:38:38 -0400

    neuroner for Twilight

---
+++ neuroner.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110205_0_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110205_1_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110205_2_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110205_3_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110205_4_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110205_5_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_0_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_1_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_2_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_3_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_4_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_5_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110206_6_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_0_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_1_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_2_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_3_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_4_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_5_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110207_6_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_0_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_1_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_2_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_3_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_4_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_5_input --pretrained_model_folder=trained_models/conll_2003_en
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/20110208_6_input --pretrained_model_folder=trained_models/conll_2003_en
+
+
---
+++ neuroner_1M.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+#python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_0_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_1_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_2_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_3_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_4_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_5_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_6_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_7_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_8_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_9_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_10_input --pretrained_model_folder=trained_models/conll_2003_en
+
+
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_11_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_12_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_13_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_14_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_15_input --pretrained_model_folder=trained_models/conll_2003_en
+
+# python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_16_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_17_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_18_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_19_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_20_input --pretrained_model_folder=trained_models/conll_2003_en
+
+
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_21_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_22_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_23_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_24_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_25_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_26_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_27_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_28_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_29_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_30_input --pretrained_model_folder=trained_models/conll_2003_en
+
+
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_31_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_32_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_33_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_34_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_35_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_36_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_37_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_38_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_39_input --pretrained_model_folder=trained_models/conll_2003_en
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_40_input --pretrained_model_folder=trained_models/conll_2003_en
+
+
+python3 __main__.py  --train_model=False --use_pretrained_model=True --dataset_text_folder=data/1M_41_input --pretrained_model_folder=trained_models/conll_2003_en
+
